98af6344de566fa59b205dfbefb6e4576345d12d
fjperez <fjperez@106-21.info2>
2022-09-21 13:35:18 +0200

    Exercise 8

---
+++ Exercise 8/src/exercise7/Exercise7.java
@@ -0,0 +1,13 @@
+package exercise7;
+
+import java.util.Scanner;
+
+public class Exercise7 {
+	public static void main(String[] args) {
+	System.out.println("Introduce tu edad guap@r");
+	Scanner sc = new Scanner (System.in);
+	double numero = sc.nextDouble() + 1;
+	System.out.println("El año que viene tendrás " + numero);
+	
+}
+}
