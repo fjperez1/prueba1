6b491a9b4acc407fa6fa9a1b7aece60061819838
fjperez <fjperez@106-21.info2>
2022-09-21 13:38:31 +0200

    Exercise 5

---
+++ Exercise 3/src/Exercise3/Exercise3.java
@@ -0,0 +1,7 @@
+package Exercise3;
+
+public class Exercise3 {
+public static void main(String[] args) {
+System.out.println("1 + 2 " + "+ 3" + " = 6");
+}
+}
---
+++ Exercise 5/src/exercise5/Exercise5.java
@@ -0,0 +1,12 @@
+package exercise5;
+
+public class Exercise5 {
+public static void main(String[] args) {
+System.out.println("Francisco Jose Perez Del Rio");
+System.out.println();
+System.out.println("Calle Verdiales");
+System.out.println();
+System.out.println("623028347");
+}
+}
+
