9007fa15d8e4d5d7140887bbb4f5fe49d46ae25a
fjperez <fjperez@106-21.info2>
2022-09-21 13:35:48 +0200

    Exercise 7

---
+++ Exercise 7/src/exercise7/Exercise7.java
@@ -0,0 +1,12 @@
+package exercise7;
+
+import java.util.Scanner;
+
+public class Exercise7 {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
+	double numero = sc.nextDouble();
+	System.out.println("Has escrito el numero" + numero);
+	
+}
+}
