e69c4aa17d356a9cc66a662a0f65ce198e5969f9
fjperez <fjperez@106-21.info2>
2022-09-21 13:36:09 +0200

    Exercise 6

---
+++ Exercise 6/src/exercise6/Exercise6.java
@@ -0,0 +1,14 @@
+package exercise6;
+
+public class Exercise6 {
+	public static void main(String[] args) {
+		System.out.println("********   *********");
+		System.out.println("**         **     **");
+		System.out.println("**         **     **");
+		System.out.println("*******    *********");
+		System.out.println("**         **");
+		System.out.println("**         **");
+		System.out.println("**         **");
+		System.out.println("**         **");
+		}
+		}
