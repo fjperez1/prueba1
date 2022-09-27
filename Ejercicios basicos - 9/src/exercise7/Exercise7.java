a8067698e7ffad9a2d1eed26ed340d7f138503d5
fjperez1 <francisco.perez@iesnervion.es>
2022-09-27 10:27:08 +0200

    a

--- Ejercicios basicos - 9/src/exercise7/Exercise7.java
+++ Ejercicios basicos - 9/src/exercise7/Exercise7.java
@@ -1,5 +1,6 @@
 package exercise7;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercise7 {
@@ -8,14 +9,24 @@ public class Exercise7 {
 		/*Realiza un conversor de pesetas a euros. Para ello, pídele al usuario que te
 		 * introduzca el valor en pesetas y, a posteriori, debes mostrarle el resultado
 		 * de la conversión.*/
+		
+		//Inicializamos las variables
+		float valorPesetas;
+		float valorEuros;
+		
+		//Pedimos en valor en pesetas al usuario
 
-		System.out.println("Introduce tu nombre");
-		Scanner nom = new Scanner(System.in);
-		String nombre = nom.nextLine();
-		System.out.println("Introduce tu edad");
-		Scanner ed = new Scanner(System.in);
-		int edad = ed.nextInt();
-		System.out.println("Hola " + nombre + ", tienes " + edad + " años ¡qué mayor eres!");
+		System.out.println("Introduce el valor en pesetas");
+		Scanner valor = new Scanner(System.in);
+		valorPesetas = valor.nextFloat();
+		
+		//Hacemos la conversión, añadimos una f al final para indicar que reduzca el numero de decimales
+		
+		valorEuros = valorPesetas/166.386f;
+		
+		//Mostramos el resultado al usuario
+		
+		System.out.println("La cantidad que has introducido es igual a " + valorEuros +" euros");
 
 	}
 }
